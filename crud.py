5e3e0685633f7051b0b236421585d7ce8cf23a3f
Josseth Rodriguez <hfernando382@gmail.com>
2022-12-12 15:08:32 -0400

    build: adding main var to storage value for db

--- crud.py
+++ crud.py
@@ -7,3 +7,16 @@ from tkinter import ttk
 
 import sqlite3
 
+# GUI setup:
+
+root = Tk()
+root.title("SQlite App")
+root.geometry("600x350")
+
+# Variables:
+
+employeeID = StringVar()
+employeeName = StringVar()
+employeeJobTitle = StringVar()
+employeeSalary = StringVar()
+
